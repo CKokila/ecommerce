b8caef0b91b82f99925979e7e6dff026905b2bc1
CKokila <kokitnl10@gmail.com>
2025-01-30 17:19:14 +0530

    bloc provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:ecommerce/const/color.dart';
 import 'package:ecommerce/routing/app_router.dart';
+import 'package:ecommerce/ui/cart/bloc/cart_bloc.dart';
+import 'package:ecommerce/ui/home/bloc/home_bloc.dart';
+import 'package:ecommerce/ui/login/bloc/login_bloc.dart';
+import 'package:ecommerce/ui/product_detail/bloc/product_bloc.dart';
+import 'package:ecommerce/ui/product_listing/bloc/product_listing_bloc.dart';
 import 'package:ecommerce/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'data/prefs/current_user.dart';
 
@@ -27,6 +33,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MultiBlocListener(
+        listeners: [
+          BlocProvider(create: (context) => LoginBloc()),
+          BlocProvider(create: (context) => HomeBloc()),
+          BlocProvider(create: (context) => ProductListingBloc()),
+          BlocProvider(create: (context) => ProductBloc()),
+          BlocProvider(create: (context) => CartBloc()),
+        ],
+        child: MaterialApp.router(
+          title: 'Ecommerce',
+          routerConfig: appRouter.config(),
+          themeMode: ThemeMode.light,
+          theme: MyTheme.lightTheme,
+          debugShowCheckedModeBanner: false,
+        ));
     return MaterialApp.router(
       title: 'Ecommerce',
       routerConfig: appRouter.config(),
